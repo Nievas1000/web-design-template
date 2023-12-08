ab299ae13aacfa3c29b4674c966ffabb8d9ce8d7
Lautaro Nievas <lautynievas09@gmail.com>
2023-12-07 23:18:59 -0300

    carousel to services added

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,31 +1,35 @@
-import React from "react";
+import React from 'react';
+import Slider from 'react-slick';
+import { carousetSettings } from '../utils/carouselSettings';
 
-export const Services = (props) => {
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+const Services = (props) => {
+
   return (
     <div id="services" className="text-center">
       <div className="container">
         <div className="section-title">
           <h2>Our Services</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          Discover a spectrum of tailored services that redefine digital excellence. From cutting-edge web development to seamless user experiences, our solutions empower businesses for success in today's dynamic digital landscape. Elevate your brand with our innovative and client-centric services, meticulously crafted for optimal impact and growth
           </p>
         </div>
-        <div className="row">
-          {props.data
-            ? props.data.map((d, i) => (
+        <Slider {...carousetSettings}>
+          { props?.data?.map((d, i) => (
                 <div key={`${d.name}-${i}`} className="col-md-4">
-                  {" "}
                   <i className={d.icon}></i>
                   <div className="service-desc">
                     <h3>{d.name}</h3>
                     <p>{d.text}</p>
                   </div>
                 </div>
-              ))
-            : "loading"}
-        </div>
+              ))}
+        </Slider>
       </div>
     </div>
   );
 };
+
+export default Services;
---
+++ src/utils/carouselSettings.js
@@ -0,0 +1,24 @@
+export const carousetSettings = {
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+};
